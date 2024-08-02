9c6e8face1ccad1290466902f7b53b50d3bd9d10
Andres Fernando Paez Cardenas <andres.paez+Siigo@siigo.com>
2024-08-02 11:07:28 -0500

    seacrh

--- src/pages/ShopCategory.jsx
+++ src/pages/ShopCategory.jsx
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react'
-import './CSS/ShopCategory.css'
-import { ShopContext } from '../context/ShopContext'
-import dropdown_icon from '../assets/dropdown_icon.png'
-import Item from '../components/Item/Item'
+import React, { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../context/ShopContext";
+import dropdown_icon from "../assets/dropdown_icon.png";
+import Item from "../components/Item/Item";
 
 const ShopCategory = (props) => {
-  const {all_product} = useContext(ShopContext);
+  const { all_product } = useContext(ShopContext);
   return (
-    <div className='shop-category'>
-      <img className='shopcategory-banner' src={props.banner} alt="" />
+    <div className="shop-category">
+      <img className="shopcategory-banner" src={props.banner} alt="" />
       <div className="shopcategory-indexSort">
         <p>
           <span>Showing 1-12</span> out of 36 products
@@ -17,21 +17,54 @@ const ShopCategory = (props) => {
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
+      <div class="max-w-md mx-auto">
+        <div class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
+          <div class="grid place-items-center h-full w-12 text-gray-300">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+
+          <input
+            class="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
+            type="text"
+            id="search"
+            placeholder="Escribe el producto.."
+          />
+        </div>
+      </div>
       <div className="shopcategory-products">
-        {all_product.map((item,i)=>{
-          if (props.category===item.category) {
-            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}/>
-          }
-          else{
+        {all_product.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
             return null;
           }
         })}
       </div>
-      <div className="shopcategory-loadmore">
-        Explorar mas
-      </div>
-    </div> 
-  )
-}
+      <div className="shopcategory-loadmore">Explorar mas</div>
+    </div>
+  );
+};
 
-export default ShopCategory
+export default ShopCategory;
