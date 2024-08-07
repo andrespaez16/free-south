bd9ec871325b4dfc52199bc57c6f268790702ac7
Andres Fernando Paez Cardenas <andres.paez+Siigo@siigo.com>
2024-08-06 20:10:51 -0500

    fix banner home

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,8 +5,8 @@ import "./hero.css";
 
 const Hero = () => {
   return (
-    <div className="text-white" id="home">
-      <div className="max-w-[700px] max-h-[850px] mt-[0px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+    <div className="" id="home">
+      <div className=" w-full h-screen mx-auto text-white bg-cover md: bg-top h-[80vh]">
  
       </div>
      
